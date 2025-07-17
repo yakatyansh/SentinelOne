fbfe3a310ca109a5d4dbb80c3142822aadd9471f
yakatyansh <ykpetrolhead08@outlook.com>
2025-07-17 23:38:17 +0530

    report starts

--- cogs/reports.py
+++ cogs/reports.py
@@ -1,7 +1,14 @@
 import discord
 from discord.ext import commands
+import asyncio
 
 
 class ReportSystem(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_raw_reaction_add(self, payload):
+        if payload.emoji.name == '🆘':
+            return
+        
